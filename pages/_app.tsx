ff053b101ff6f180989f2abaee3734930b37ce0d
Jaecobd1 <jaecob.d1@gmail.com>
2022-11-05 20:00:08 -0400

    adding nextjs-googleanalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,17 @@ import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import Script from 'next/script'
+import { GoogleAnalytics } from "nextjs-google-analytics"
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      
+      <GoogleAnalytics trackPageViews />
       <Nav />
       <Component className="z-0"{...pageProps} />
       <Footer />
-      <Script  src = "https://www.googletagmanager.com/gtag/js?id=G-4D9LGJVHQQ" id="Gtag"> </Script> 
-      <Script src="../scripts/googleAnalytics.js" id="gAnalytics"></Script>
-
+      
       </>
   )
 }

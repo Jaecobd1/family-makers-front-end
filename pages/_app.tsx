f26d276e5952f33f99fcdbd60bd773cd8505ab3d
Jaecobd1 <jaecob.d1@gmail.com>
2022-11-06 13:26:03 -0500

    fixing google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,21 @@ import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import Script from 'next/script'
-import { GoogleAnalytics } from "nextjs-google-analytics"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics trackPageViews />
+      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy='lazyOnload'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
       <Nav />
       <Component className="z-0"{...pageProps} />
       <Footer />

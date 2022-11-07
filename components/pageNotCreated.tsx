54db3bf530aff9f5f1c02d24421a49e64d28ff98
Jaecobd1 <jaecob.d1@gmail.com>
2022-11-07 14:30:18 -0500

    updating page not found message

--- components/pageNotCreated.tsx
+++ components/pageNotCreated.tsx
@@ -1,7 +1,7 @@
 function PageNotCreated() {
     return (
         <div className="h-screen text-black flex justify-center items-center">
-            <p>This page will be updated in 2023-2024 after our research provides online engineering programs.</p>
+            <p>This page will be updated in 2023-2024 after our research completes the online engineering programs.</p>
         </div>
     )
 }
